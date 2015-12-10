b3378e015fe4e4d39cfeb1163a4e7ebde6dfc63d
Jirayu <Jirayu@10.201.182.61>
2015-12-10 15:34:27 +0700

    GameScreen

--- src/ui/GameScreen.java
+++ src/ui/GameScreen.java
@@ -3,6 +3,8 @@ package ui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
@@ -14,11 +16,16 @@ public class GameScreen extends JPanel{
 	 * 
 	 */
 	private static final long serialVersionUID = -8593735507163883962L;
+	private Map currentMap=new Map(1,configs.cblack,configs.cpink);
 	public GameScreen(){
+		super();
 		this.setLayout(new BorderLayout());
 		this.setPreferredSize(new Dimension(configs.screenWidth,configs.screedHeight));
 		this.setBackground(configs.cblack);
-		
+		setDoubleBuffered(true);
+		setVisible(true);
+		setFocusable(true);
+		requestFocus();
 		///////// Title Panel
 		JPanel TitlePanel=new JPanel();
 		TitlePanel.setBackground(configs.cblack);
@@ -31,7 +38,7 @@ public class GameScreen extends JPanel{
 		JPanel MapPanel=new JPanel();
 		JPanel CenterPanel = new JPanel();
 		CenterPanel.setLayout(new BorderLayout());
-		MapPanel.setBackground(configs.cpink);
+		MapPanel.setBackground(Color.red);
 		MapEastPanel.setBackground(configs.cblack);
 		MapWestPanel.setBackground(configs.cblack);
 		MapPanel.setPreferredSize(new Dimension(configs.mapWidth,configs.mapHeight));
@@ -40,7 +47,7 @@ public class GameScreen extends JPanel{
 		MapEastPanel.setPreferredSize(new Dimension(sideWidth,sideHeight));
 		MapWestPanel.setPreferredSize(new Dimension(sideWidth,sideHeight));
 		CenterPanel.add(MapWestPanel,BorderLayout.WEST);
-		CenterPanel.add(MapPanel,BorderLayout.CENTER);
+		//CenterPanel.add(MapPanel,BorderLayout.CENTER);
 		CenterPanel.add(MapEastPanel,BorderLayout.EAST);
 		
 		///////// Level Panel
@@ -50,8 +57,24 @@ public class GameScreen extends JPanel{
 		
 		
 		this.add(TitlePanel, BorderLayout.NORTH);
-		this.add(CenterPanel, BorderLayout.CENTER);
+		//this.add(CenterPanel, BorderLayout.CENTER);
 		this.add(LevelPanel, BorderLayout.SOUTH);
+		this.setVisible(true);
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		// TODO Auto-generated method stub
+		
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D)g;
+		g2d.setBackground(Color.white);
+		g2d.clearRect(0, 0, configs.screenWidth, configs.screedHeight);
+		g2d.setColor(configs.cblack);
+		g2d.fillRect(0, configs.otherPanelHeight, configs.screenWidth, configs.mapHeight);
+		if(currentMap.isVisible()) currentMap.draw(g2d);
+		//System.out.println(111);
+		
 	}
 	
 }
