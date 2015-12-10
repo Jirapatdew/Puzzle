e2f75cc1afd06ed4b9b09010218ae9e41b0ef582
Jirayu <Jirayu@10.201.182.61>
2015-12-10 13:52:21 +0700

    add configs

--- src/config/configs.java
+++ src/config/configs.java
@@ -13,6 +13,15 @@ public class configs {
 	public static int mapHeight=mapGridHeight*singleHeight;
 	public static int otherPanelWidth=640;
 	public static int otherPanelHeight=120;
+	
+	
 	public static Color cblack=new Color(59,64,67);
 	public static Color cpink=new Color(251,97,105);
+	
+	
+	public static int PLAYER=4;
+	public static int PASSABLE_TERRAIN=0;
+	public static int UNPASSABLE_TERRAIN=1;
+	public static int MOVABLE_BLOCK=2;
+	public static int DESTRUCTIBLE_BLOCK=3;
 }
